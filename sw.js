e37d254b6d6899c30fcbecdccb34def20186af70
abdulrahmanroston <119020009+abdulrahmanroston@users.noreply.github.com>
2025-03-17 20:47:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open('v1').then((cache) => {
             return cache.addAll([
-                '/index.html',
+                'https://abdulrahmanroston.github.io/KabbaniSecurity/index.html',
                 'Icon1.png'
             ]);
         })
